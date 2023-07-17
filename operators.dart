fe6467146187bd539ee56a4689fe6dd2907d0199
GinilabDev <80915429+GinilabDev@users.noreply.github.com>
2023-07-17 18:16:58 +0600

    Dart Operators

---
+++ operators.dart
@@ -0,0 +1,30 @@
+void main() {
+  /**
+  1. arithmatic operator 
+  ex:{+, -, *, /, ~/, %, ++, --}
+
+  2. equality and relational operator
+  Ex: {>, <, >=, <=, ==, !=}
+
+  3. Type test operator
+  Ex: (is, is!)
+
+  4. bitwise operator
+  Ex: (&, |, ^, ~, <<, >>)
+
+  5. assignment operator
+  Ex: {=, ??, +=, -=, *=, /=}
+
+  6. conditional operator
+  Ex: {??, ?, ;, if, else, elseif}
+
+  7. logical operator
+  Ex: {&&, ||, !}
+  8. cascade operator
+  Ex: {..}
+
+  cheatseet: https://www.figma.com/file/Fov4xJPD9kTxWb1IUupIbi/Dart-Operators?type=design&node-id=0-1&mode=design
+
+  NOTE: google bard or chat GPT can help
+*/
+}
