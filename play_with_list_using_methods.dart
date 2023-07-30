1fda51fe8f9ea4fa576010652cc074f848cabcc0
GinilabDev <80915429+GinilabDev@users.noreply.github.com>
2023-07-30 11:07:41 +0600

    pay with list using methods

--- play_with_list_using_methods.dart
+++ play_with_list_using_methods.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 void main() {
   /**
    * usefull methods
@@ -114,4 +116,128 @@ void main() {
   print('Example of "indexOf"');
   print(list12.indexOf('Sohel'));
   print("\n");
+
+  /**indexOf */
+  String myVar1 = "banana";
+  print('2nd Example of "indexOf"');
+  print(myVar1.indexOf('a'));
+  print("\n");
+
+  /**lastIndexOf */
+  String myVar2 = "banana";
+  print('Example of "lastIndexOf"');
+  print(myVar2.lastIndexOf('a'));
+  print("\n");
+
+  /**first */
+  List<String> list13 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "first"');
+  print(list13.first);
+  print("\n");
+
+  /**last */
+  List<String> list14 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "last"');
+  print(list14.last);
+  print("\n");
+
+  /**reversed */
+  List<String> list15 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "reversed"');
+  print(list15.reversed);
+  print("\n");
+
+  /**asMap */
+  List<String> list16 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "asMap"');
+  print(list16.asMap());
+  print("\n");
+
+  /**whereType<datatype>() */
+  List<dynamic> list17 = ["Helal", 2, "Sohel", 3, "Tuhin", 4];
+  Iterable<int> intsx = list17.whereType<int>();
+  print('Example of "whereType"');
+  for (var e in intsx) {
+    print(e);
+  }
+  print("\n");
+
+  /**getRange */
+  List<String> list18 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "getRange"');
+  print(list18.getRange(0, 2));
+  print("\n");
+
+  /**replaceRange */
+  List<String> list19 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  list19.replaceRange(3, 3, ["Raihan", "Mohon"]);
+  print('Example of "replaceRange"');
+  print(list19);
+  print("\n");
+
+  /**skip */
+  List<String> list20 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "skip"');
+  print(list20.skip(2));
+  print("\n");
+
+  /**take */
+  List<String> list21 = ["Helal", "Sohel", "Tuhin", "Rakib"];
+  print('Example of "take"');
+  print(list21.take(3));
+  print("\n");
+
+  /**every */
+  List<int> list22 = [1, 2, 3, 4, 5];
+  print('Example of "every"');
+  bool allEven = list22.every((list22) => list22 % 2 == 0);
+  print(allEven); // false
+
+  bool allEven2 = list22.every((list22) => list22 > 0);
+  print(allEven2); // true
+  print("\n");
+
+  /**any */
+  List<int> list23 = [1, 2, 3, 4, 5];
+  print('Example of "any"');
+  bool hasEven = list23.any((list23) => list23 % 2 == 0);
+  print(hasEven); // false
+
+  bool hasEven2 = list23.any((list23) => list23 == 10);
+  print(hasEven2);
+  print("\n");
+
+  /**followedBy */
+  // simillar as addAll
+
+  /**reduce */
+  List<int> list24 = [1, 2, 3, 4, 5];
+  print('Example of "reduce"');
+  print(list24.reduce((value, element) => value + element)); // true
+  print("\n");
+
+  /**fold */
+  List<int> list25 = [1, 2, 3, 4, 5];
+  int sum = list25.fold(
+    0,
+    (previousValue, element) => previousValue + element,
+  );
+  print('Example of "fold"');
+  print(sum);
+  print("\n");
+
+  /**singleWere */
+  // List<int> list26 = [1, 2, 3, 4, 5];
+  // int evenNumber = list26.singleWhere((e) => e % 2 == 0);
+  // print('Example of "singleWhere"');
+  // print(evenNumber);
+  // print("\n");
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Find the first even number using singleWhere()
+  int evenNumber = numbers.singleWhere((number) => number % 2 == 0);
+
+  // Print the even number
+  print(evenNumber); // 2
 }
